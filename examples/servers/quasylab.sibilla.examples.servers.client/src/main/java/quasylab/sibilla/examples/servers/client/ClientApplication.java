f8333a2e2cf0eb6159d41aa5b7c84589330a95de
ZamponiMarco <zamponi.marco3@gmail.com>
2020-04-21 19:53:28 +0200

    [UPDATE] Disabled CORS for dev purposes

--- examples/servers/quasylab.sibilla.examples.servers.client/src/main/java/quasylab/sibilla/examples/servers/client/ClientApplication.java
+++ examples/servers/quasylab.sibilla.examples.servers.client/src/main/java/quasylab/sibilla/examples/servers/client/ClientApplication.java
@@ -39,7 +39,7 @@ public class ClientApplication implements Serializable {
      *
      */
     private static final long serialVersionUID = 1L;
-    private static final int REPLICA = 10;
+    private static final int REPLICA = 10000;
 
     private static final AbstractRandomGenerator RANDOM_GENERATOR = new DefaultRandomGenerator();
     private static final String MODEL_NAME = ClientApplication.class.getName();
