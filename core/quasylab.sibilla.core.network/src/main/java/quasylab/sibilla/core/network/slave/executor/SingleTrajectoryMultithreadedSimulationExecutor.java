d1570d2756f7ef9d18b8af73fc77adedc6021e7d
jummes <32539635+ZamponiMarco@users.noreply.github.com>
2020-09-09 17:22:45 +0200

    [UPDATE] Restored initial implementation of SingleTrajectoryMultithreadedSimulationExecutor

--- core/quasylab.sibilla.core.network/src/main/java/quasylab/sibilla/core/network/slave/executor/SingleTrajectoryMultithreadedSimulationExecutor.java
+++ core/quasylab.sibilla.core.network/src/main/java/quasylab/sibilla/core/network/slave/executor/SingleTrajectoryMultithreadedSimulationExecutor.java
@@ -10,9 +10,9 @@ import quasylab.sibilla.core.simulator.Trajectory;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.stream.IntStream;
 
 public class SingleTrajectoryMultithreadedSimulationExecutor extends SimulationExecutor {
 
@@ -26,10 +26,10 @@ public class SingleTrajectoryMultithreadedSimulationExecutor extends SimulationE
     public void simulate(NetworkTask networkTask, TCPNetworkManager master) {
         List<? extends SimulationTask<?>> tasks = networkTask.getTasks();
         Model model = tasks.get(0).getUnit().getModel();
-
-        /*
         LinkedList<Trajectory<?>> trajectories = new LinkedList<>();
         CompletableFuture<?>[] futures = new CompletableFuture<?>[tasks.size()];
+
+        /*
 
         this.computationBenchmark.run(() -> {
             for (int i = 0; i < tasks.size(); i++) {
@@ -48,11 +48,17 @@ public class SingleTrajectoryMultithreadedSimulationExecutor extends SimulationE
         }
          */
 
-        IntStream.range(0, tasks.size()).forEach(i -> {
-            taskExecutor.execute(() -> {
-                Trajectory trajectory = tasks.get(i).get();
-                sendResult(new ComputationResult(new LinkedList<>(List.of(trajectory))), master, model);
-            });
-        });
+        for (int i = 0; i < tasks.size(); i++) {
+            futures[i] = CompletableFuture.supplyAsync(tasks.get(i), taskExecutor);
+        }
+        CompletableFuture.allOf(futures).join();
+        for (SimulationTask<?> task : tasks) {
+            Trajectory trajectory = task.getTrajectory();
+            trajectories.add(trajectory);
+        }
+
+        for (Trajectory singleTrajectory : trajectories) {
+            sendResult(new ComputationResult(new LinkedList<>(List.of(singleTrajectory))), master, model);
+        }
     }
 }
