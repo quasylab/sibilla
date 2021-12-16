ddb0fbdbcd3852d4db82f53d626b07d1d4a52c1d
Filippo Biondi <filippo.biondi@protonmail.ch>
2021-12-16 19:49:32 +0100

    Fixed setup.py

--- shell/src/dist/scripts/sibilla_py/setup.py
+++ shell/src/dist/scripts/sibilla_py/setup.py
@@ -5,9 +5,6 @@ setup(
     name='sibilla',
     version='1.0',
     packages=['sibilla'],
-    data_files=[
-        ('libs', glob('../../build/install/sshell/lib/**'))
-    ],
     install_requires=[
         'Cython',
         'pyjnius',
