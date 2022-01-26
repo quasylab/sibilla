09f8d15bfa761d7a091b1c857397091661eb0889
LorenzoMatteucci <80180571+LorenzoMatteucci@users.noreply.github.com>
2022-01-26 13:22:20 +0100

    changed seir model

--- shell/src/dist/examples/seir/seir.pm
+++ shell/src/dist/examples/seir/seir.pm
@@ -1,18 +1,25 @@
+param lambdaMeet = 1.0; /* Meeting rate */
+param lambdaExposure = 1.00; /* rate of Exposure */
+param lambdaInfection = 2.00; /* rate of Infection */
+param lambdaRecovery = 0.5; /* rate of recovery */
+
+const startS = 9; /* Initial number of S agents */
+const startI = 1; /* Initial number of I agents */
+
 species S;
 species E;
 species I;
 species R;
 
-rule s_to_e {
-    S|I -[ 1.0*%I ]-> E|I
+rule exposure {
+    S|I -[ #S *% I* lambdaMeet * lambdaExposure ]-> E|I
+}
+rule infection {
+    E -[ #E* lambdaInfection ]-> I
+}
+rule recovered {
+    I -[ #I* lambdaRecovery ]-> R
 }
 
-rule e_to_i {
-    E -[ 1.0 ]-> I
-}
-
-rule i_to_r {
-    I -[ 1.0 ]-> R
-}
-
-system init = S<10>|I;
+system initial = S < startS >|I < startI >;
+predicate allRecovered = (# S +# E +# I ==0) ;
